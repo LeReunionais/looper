29b7fff7c4eb2561a97530b6e628c2fee4d97883
LeReunionais <reunionaisle@gmail.com>
2016-03-28 12:35:14 +0800

    Register world publication

--- looper.go
+++ looper.go
@@ -3,15 +3,18 @@ package main
 import (
 	"github.com/LeReunionais/looper/interface"
 	"github.com/LeReunionais/looper/world"
+	"github.com/LeReunionais/service"
 	"log"
+	"os"
 	"time"
 )
 
 func main() {
 	w := world.Init()
-	log.Printf("looper")
+	log.Printf("Looper - world initialized")
 	start := time.Now()
 	go interfaces.Publish("tcp", 6000, &w)
+	register()
 	for {
 		time.Sleep(100 * time.Millisecond)
 		end := time.Now()
@@ -19,4 +22,31 @@ func main() {
 		w.Update(delta)
 		start = end
 	}
+}
+
+func register() {
+	host, registry := extract_env()
+	endpoint := "tcp://" + registry + ":3001"
+	port := 6000
+	world := service.Service{
+		"world",
+		host,
+		"PUB",
+		port,
+	}
+	service.Register(endpoint, world)
+}
+
+func extract_env() (host, registry string) {
+	host = os.Getenv("SERVICE_HOST")
+	if host == "" {
+		log.Fatal("Environment variable SERVICE_HOST is not defined. Please set it up. This variable defined the host on which is published the service")
+	}
+
+	registry = os.Getenv("REGISTRY_HOST")
+	if registry == "" {
+		log.Fatal("Environment variable REGISTRY_HOST is not defined. Please set it up. Variable defined registry host.")
+	}
+
+	return
 }
